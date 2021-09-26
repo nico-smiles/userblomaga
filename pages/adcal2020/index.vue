9c8fd690abf9e616e2327b86648891fa5e9bb1b9
AyumuNekozuki <ayumu@nekozuki.me>
2021-09-26 19:16:26 +0900

    表のリンクを修正

--- pages/adcal2020/index.vue
+++ pages/adcal2020/index.vue
@@ -30,72 +30,74 @@
             <tbody>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/04</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://ch.nicovideo.jp/smiles/blomaga/ar1971196" rel="noopener noreferrer">（告知記事）「SmileSアドベントカレンダー」開催します！</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;">
+                  <a style="color:#0080ff;" href="https://ch.nicovideo.jp/smiles/blomaga/ar1971196" rel="noopener noreferrer">（告知記事）「SmileSアドベントカレンダー」開催します！</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">CHブロマガ</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/05</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/05" rel="noopener noreferrer">No.28のおすすめコンテンツ</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;">
+                  <a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/5" rel="noopener noreferrer">No.28のおすすめコンテンツ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">猫月遥歩</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/06</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">少しゾクッとするコンテンツ</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/6" rel="noopener noreferrer">少しゾクッとするコンテンツ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.27</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/07</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">No.118のおすすめコンテンツ</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/7" rel="noopener noreferrer">No.118のおすすめコンテンツ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.118</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/08</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">リモートならではのコンテンツ</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/8" rel="noopener noreferrer">リモートならではのコンテンツ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.27</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/09</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part1</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/9" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part1</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.2</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/10</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">旅行動画を見るとカメラ機材が欲しくなってしまうんだ2020</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/10" rel="noopener noreferrer">旅行動画を見るとカメラ機材が欲しくなってしまうんだ2020</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.12</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/11</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">手描きにじさんじシリーズ名作集2020</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/11" rel="noopener noreferrer">手描きにじさんじシリーズ名作集2020</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">お茶</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/12</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">ニコニコ建国記念日なので2020年のニコニコを振り返ってみた</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/12" rel="noopener noreferrer">ニコニコ建国記念日なので2020年のニコニコを振り返ってみた</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.28</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/13</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">私の手紙と、劇場版ヴァイオレット・エヴァーガーデン</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/13" rel="noopener noreferrer">私の手紙と、劇場版ヴァイオレット・エヴァーガーデン</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.99</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/14</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part2</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/14" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part2</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.2</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/15</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">料理動画あつめてみた</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/15" rel="noopener noreferrer">料理動画あつめてみた</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.102</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/16</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">読み切り！ボイロノヒジキタイムズ</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/16" rel="noopener noreferrer">読み切り！ボイロノヒジキタイムズ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.88</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/17</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part3</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/17" rel="noopener noreferrer">なぜ詳しいし。斜め上のテーマがいっぱいなニコニコ解説タグを紹介！ Part3</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.2</td>
               </tr>
               <tr>
@@ -110,7 +112,7 @@
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/20</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">「これぞニコニコらしさ！」という動画をピックアップ！</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/20" rel="noopener noreferrer">「これぞニコニコらしさ！」という動画をピックアップ！</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.48</td>
               </tr>
               <tr>
@@ -120,12 +122,12 @@
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/22</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">これが私流リア充</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/22" rel="noopener noreferrer">これが私流リア充</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.11</td>
               </tr>
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">12/23</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">スクリプトアート動画紹介</a></td>
+                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://nico-smiles.github.io/userblomaga/adcal2020/23" rel="noopener noreferrer">スクリプトアート動画紹介</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">No.43</td>
               </tr>
               <tr>
@@ -141,11 +143,6 @@
               <tr>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">01/13</td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="https://ch.nicovideo.jp/smiles/blomaga/ar1984624" rel="noopener noreferrer">2020年のエージェントおすすめコンテンツを全てまとめてみた</a></td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;">CHブロマガ</td>
-              </tr>
-              <tr>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;">10/XX</td>
-                <td style="padding:5px; border-bottom: #ccc 1px solid;"><a style="color:#0080ff;" href="" rel="noopener noreferrer">『SmileSアドベントカレンダー2020』記事移動のお知らせ</a></td>
                 <td style="padding:5px; border-bottom: #ccc 1px solid;">CHブロマガ</td>
               </tr>
             </tbody>
